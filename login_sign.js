8ec190726a89c9731f6ffe336a3880397184c7e0
Anand kumar pg <Anandkumarpg359@gmail.com>
2023-11-20 21:49:56 +0530

    third commit

---
+++ login_sign.js
@@ -0,0 +1,13 @@
+function signup() {
+  // signup
+    var username = document.getElementById('signupUsername').value;
+    var password = document.getElementById('signupPassword').value;
+    alert('Signup successful!'); 
+}
+
+function login() {
+    //login
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
+    alert('Login successful!');
+}
